427dad418167dac10968df4b31d140baf7d36455
khenderson96 <khenderson614@gmail.com>
2025-10-28 13:07:41 -0400

    reenable patch save/recall buttons

--- video_synth/save.py
+++ video_synth/save.py
@@ -44,81 +44,7 @@ class SaveController:
         return save_dir_path
 
 
-    def on_fwd_button_click(self):
-
-        print(f"Forward button clicked!")
-
-        # Read existing YAML data if the file exists
-        if os.path.exists(self.yaml_file_path):
-            try:
-                with open(self.yaml_file_path, 'r') as f:
-                    saved_values = list(yaml.safe_load_all(f))
-                    print(saved_values)
-
-                self.index = (self.index + 1) % len(saved_values)
-                self.load_param_vals(saved_values)
-                print(f"Successfully loaded existing YAML from: {self.yaml_file_path}")
-            except yaml.YAMLError as e:
-                print(f"Error loading YAML file {self.yaml_file_path}: {e}")
-                return
-            except Exception as e:
-                print(f"An unexpected error occurred while reading {self.yaml_file_path}: {e}")
-                return
-        else:
-            print(f"YAML file not found at {self.yaml_file_path}. A new one will be created.")
-
-
-    def on_prev_button_click(self):
-
-        log.debug(f"Prev button clicked!")
-
-        try:
-            with open("saved_values.yaml", "r") as f:
-                saved_values = list(yaml.safe_load_all(f))
-
-            self.index = (self.index - 1) % len(saved_values[0])
-            self.load_param_vals(saved_values)
-
-        except Exception as e:
-            log.exception(f"Error loading values: {e}")
-
-
-    def on_rand_button_click(self):
-        log.debug(f"Random button clicked!")
-    
-        # get values from saved_values.yaml
-        try:
-            with open("saved_values.yaml", "r") as f:
-                saved_values = list(yaml.safe_load_all(f))
-
-            self.index = random.randint(0, len(saved_values[0]) - 1)
-            self.load_param_vals(saved_values)
-            
-        except Exception as e:
-            print(f"Error loading values: {e}")
-
-
-    def load_param_vals(self, saved_values):
-        d = saved_values[0][self.index]
-        print(f"loaded values at index {self.index}: {d}\n\n")
-        for param_name in self.params.keys():
-            for tag in d.keys():
-                if tag == param_name:
-                    self.params[param_name].set(d[tag])
-                    dpg.set_value(param_name, d[tag])
-
-
-    def get_values(self):
-        new_data = {}
-        for param_name, param_obj in self.params.items():
-            if isinstance(param_obj, Param):
-                new_data[param_name] = param_obj.value
-            else:
-                log.warning(f"{param_name} is not a Param object, cannot save value")
-        print(new_data)
-        return new_data
-
-    def save2(self):
+    def on_save_button_click(self, app_data, user_data):
         ROOT_KEY = 'entries'
         current_yaml_data = {}
 
@@ -127,7 +53,6 @@ class SaveController:
             try:
                 with open(self.yaml_file_path, 'r') as f:
                     current_yaml_data = yaml.safe_load(f)
-                log.debug(f"Existing data loaded: {current_yaml_data}")
             except yaml.YAMLError as e:
                 log.exception(f"Could not read existing YAML data: {e}. Starting with empty data.")
                 current_yaml_data = {}
@@ -147,13 +72,54 @@ class SaveController:
         with open(self.yaml_file_path, 'w') as file:
             yaml.dump(current_yaml_data, file, default_flow_style=False, sort_keys=False)
             
-        return current_yaml_data
 
-    def on_save_button_click(self, appdata, userdata):
-        self.save2()
-        
-        # TODO: determine best way to grap a frame for saving w/o having to pass into method
-        # cv2.imwrite(f"{date_time_str}.jpg", frame)
+    def load_button_callback(self, sender, app_data, user_data):
+
+        # Read existing YAML data if the file exists
+        if os.path.exists(self.yaml_file_path):
+            try:
+                with open(self.yaml_file_path, 'r') as f:
+                    saved_values = list(yaml.safe_load_all(f))
+
+                if user_data == self.fwd.tag:
+                    self.index = (self.index + 1) % len(saved_values)
+                elif user_data == self.prev.tag:
+                    self.index = (self.index - 1) % len(saved_values)
+                else:
+                    self.index = random.randint(0, len(saved_values[0]) - 1)
+
+                parsed_values = saved_values[0]['entries'][self.index]
+                self.load_param_vals(parsed_values)
+
+                log.info(f"Successfully loaded save index ({self.index}) from: {self.yaml_file_path}")
+
+            except yaml.YAMLError as e:
+                print(f"Error loading YAML file {self.yaml_file_path}: {e}")
+                return
+            except Exception as e:
+                print(f"An unexpected error occurred while reading {self.yaml_file_path}: {e}")
+                return
+        else:
+            print(f"YAML file not found at {self.yaml_file_path}. A new one will be created.")
+
+
+    def load_param_vals(self, parsed_param_dict):
+        for param_name in self.params.keys():
+            for tag in parsed_param_dict.keys():
+                if tag == param_name:
+                    self.params[param_name].value = parsed_param_dict[tag]
+                    # dpg.set_value(param_name, parsed_param_dict[tag])
+
+
+    def get_values(self):
+        new_data = {}
+        for param_name, param_obj in self.params.items():
+            if isinstance(param_obj, Param):
+                new_data[param_name] = param_obj.value
+            else:
+                log.warning(f"{param_name} is not a Param object, cannot save value")
+        print(new_data)
+        return new_data
 
     def create_save_buttons(self):
         """
@@ -163,11 +129,11 @@ class SaveController:
 
         with dpg.group(horizontal=True):
             dpg.add_button(label=self.save.label, callback=self.on_save_button_click, user_data=self.save.tag, width=width//3)
-            dpg.add_button(label=self.fwd.label, callback=self.on_fwd_button_click, user_data=self.fwd.tag, width=width//3)
-            dpg.add_button(label=self.prev.label, callback=self.on_prev_button_click, user_data=self.prev.tag, width=width//3)
+            dpg.add_button(label=self.fwd.label, callback=self.load_button_callback, user_data=self.fwd.tag, width=width//3)
+            dpg.add_button(label=self.prev.label, callback=self.load_button_callback, user_data=self.prev.tag, width=width//3)
 
         with dpg.group(horizontal=True):
-            dpg.add_button(label=self.rand.label, callback=self.on_rand_button_click, user_data=self.rand.tag, width=width//3)
+            dpg.add_button(label=self.rand.label, callback=self.load_button_callback, user_data=self.rand.tag, width=width//3)
 
 
 class Timeline:
